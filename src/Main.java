87f90b8bfd744e74d8faf0e6ded5a433f8641ae8
laboratorio <jean.peralta@udla.edu.ec>
2025-06-12 17:25:12 -0500

    1

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import figurasGeometricas.CirculoDibujable;
+import figurasGeometricas.CuadradoDibujable;
 
 import javax.swing.*;
 import java.awt.*;
@@ -7,18 +8,27 @@ public class Main {
     public static void main(String[] args) {
 
         figurasGeometricas.CirculoDibujable circulo = new CirculoDibujable(100, 0, 0);
+        figurasGeometricas.CuadradoDibujable cuadrado = new CuadradoDibujable(100,100,0);
 
         System.out.println("El perímetro del circulo es: " + circulo.getPerimetro());
         System.out.println("El área del circulo es: " + circulo.getArea());
+        System.out.println("El perímetro del cuadrado es: " + cuadrado.getPerimetro());
+        System.out.println("El área del cuadrado es: " + cuadrado.getArea());
 
 
         JFrame frame = new JFrame("Dibujar circulo");
-        JPanel panel = new JPanel(){
+        JPanel panel = new JPanel() {
+
 
         @Override
         protected void paintComponent(Graphics g){
             super.paintComponent(g);
             circulo.dibujar2D(g);
+            cuadrado.dibujar2D(g);
+
+
+
+
         }
     };
 
